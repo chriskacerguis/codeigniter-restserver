7f75af0b36561e87cdb0e6547e1e388e406148c3
philsturgeon <email@philsturgeon.co.uk>
2009-06-24 09:07:57 -0700

    Added an option to skip the HTTP Accept checking. It makes things more RESTful but if you arent using it then you could speed calls up a tiny bit.'

--- application/config/rest.php
+++ application/config/rest.php
@@ -48,6 +48,19 @@ $config['rest_valid_logins'] = array('admin' => '1234');
 */
 $config['rest_cache'] = 0;
 
+/*
+|--------------------------------------------------------------------------
+| REST Ignore HTTP Accept
+|--------------------------------------------------------------------------
+|
+| Set to TRUE to ignore the HTTP Accept and speed up each request a little.
+| Only do this if you are using the $this->rest_format or /format/xml in URLs 
+|
+|	FALSE
+|
+*/
+$config['rest_ignore_http_accept'] = FALSE;
+
 
 /* End of file config.php */
 /* Location: ./system/application/config/rest.php */
