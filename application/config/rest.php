f63915d7d00495d9baacd847b7650432b8c96866
Simon Bauman <simon333@gmail.com>
2011-01-16 11:07:42 -0800

    Added the ability to choose an overriding auth type per class/method by setting the variable 'auth_override_class_method' in the rest config.

--- application/config/rest.php
+++ application/config/rest.php
@@ -38,6 +38,29 @@ $config['rest_auth'] = '';
 
 /*
 |--------------------------------------------------------------------------
+| Override auth types for specific class/method
+|--------------------------------------------------------------------------
+|
+| Set specific authentication types for methods within a class (controller)
+|
+| Set as many config entries as needed.  Any methods not set will use the default 'rest_auth' config value.
+|
+| example:  
+| 
+|			$config['auth_override_class_method']['deals']['view'] = 'none';
+|			$config['auth_override_class_method']['deals']['insert'] = 'digest';
+|			$config['auth_override_class_method']['accounts']['user'] = 'basic'; 
+|
+| Here 'deals' and 'accounts' are controller names, 'view', 'insert' and 'user' are methods within. (NOTE: leave off the '_get' or '_post' from the end of the method name)
+| Acceptable values are; 'none', 'digest' and 'basic'.  
+|
+*/
+// $config['auth_override_class_method']['deals']['view'] = 'none';
+// $config['auth_override_class_method']['deals']['insert'] = 'digest';
+// $config['auth_override_class_method']['accounts']['user'] = 'basic';
+
+/*
+|--------------------------------------------------------------------------
 | REST Login usernames
 |--------------------------------------------------------------------------
 |
