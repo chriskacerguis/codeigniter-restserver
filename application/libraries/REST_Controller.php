f3e3f3b65d7725c3e3e8ac332537b6e9ad83cb7d
Kevin <kevin.lion@chokoala.fr>
2011-05-10 17:57:10 +0200

    Accept same type of parameters for post vars AS for put or delete vars (not only application/x-www-form-urlencoded type)

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -602,7 +602,7 @@ class REST_Controller extends CI_Controller {
 			return $this->_post_args;
 		}
 
-		return $this->input->post($key, $xss_clean);
+		return array_key_exists($key, $this->_post_args) ? $this->_xss_clean($this->_post_args[$key], $xss_clean) : FALSE;
 	}
 
 	public function put($key = NULL, $xss_clean = TRUE)
