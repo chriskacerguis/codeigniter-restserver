ea2255f020db8694789707efc81a33fdac17cb17
Mhd Zaher Ghaibeh <linux.juggler@gmail.com>
2012-01-07 13:39:00 +0200

    fixing and learning

--- application/controllers/api/example.php
+++ application/controllers/api/example.php
@@ -1,4 +1,6 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * Example
@@ -11,85 +13,68 @@
  * @category	Controller
  * @author		Phil Sturgeon
  * @link		http://philsturgeon.co.uk/code/
-*/
-
+ */
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
-require APPPATH.'/libraries/REST_Controller.php';
+require APPPATH . '/libraries/REST_Controller.php';
 
-class Example extends REST_Controller
-{
-	function user_get()
-    {
-        if(!$this->get('id'))
-        {
-        	$this->response(NULL, 400);
+class Example extends REST_Controller {
+
+    function user_get() {
+        if (!$this->get('id')) {
+            $this->response(NULL, 400);
         }
 
         // $user = $this->some_model->getSomething( $this->get('id') );
-    	$users = array(
-			1 => array('id' => 1, 'name' => 'Some Guy', 'email' => 'example1@example.com', 'fact' => 'Loves swimming'),
-			2 => array('id' => 2, 'name' => 'Person Face', 'email' => 'example2@example.com', 'fact' => 'Has a huge face'),
-			3 => array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com', 'fact' => 'Is a Scott!', array('hobbies' => array('fartings', 'bikes'))),
-		);
-		
-    	$user = @$users[$this->get('id')];
-    	
-        if($user)
-        {
-            $this->response($user, 200); // 200 being the HTTP response code
-        }
+        $users = array(
+            1 => array('id' => 1, 'name' => 'Some Guy', 'email' => 'example1@example.com', 'fact' => 'Loves swimming'),
+            2 => array('id' => 2, 'name' => 'Person Face', 'email' => 'example2@example.com', 'fact' => 'Has a huge face'),
+            3 => array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com', 'fact' => 'Is a Scott!', array('hobbies' => array('fartings', 'bikes'))),
+        );
 
-        else
-        {
+        $user = @$users[$this->get('id')];
+
+        if ($user) {
+            $this->response($user, 200); // 200 being the HTTP response code
+        } else {
             $this->response(array('error' => 'User could not be found'), 404);
         }
     }
-    
-    function user_post()
-    {
+
+    function user_post() {
         //$this->some_model->updateUser( $this->get('id') );
         $message = array('id' => $this->get('id'), 'name' => $this->post('name'), 'email' => $this->post('email'), 'message' => 'ADDED!');
-        
+
         $this->response($message, 200); // 200 being the HTTP response code
     }
-    
-    function user_delete()
-    {
-    	//$this->some_model->deletesomething( $this->get('id') );
+
+    function user_delete() {
+        //$this->some_model->deletesomething( $this->get('id') );
         $message = array('id' => $this->get('id'), 'message' => 'DELETED!');
-        
+
         $this->response($message, 200); // 200 being the HTTP response code
     }
-    
-    function users_get()
-    {
+
+    function users_get() {
         //$users = $this->some_model->getSomething( $this->get('limit') );
         $users = array(
-			array('id' => 1, 'name' => 'Some Guy', 'email' => 'example1@example.com'),
-			array('id' => 2, 'name' => 'Person Face', 'email' => 'example2@example.com'),
-			3 => array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com', 'fact' => array('hobbies' => array('fartings', 'bikes'))),
-		);
-        
-        if($users)
-        {
-            $this->response($users, 200); // 200 being the HTTP response code
-        }
+            array('id' => 1, 'name' => 'Some Guy', 'email' => 'example1@example.com'),
+            array('id' => 2, 'name' => 'Person Face', 'email' => 'example2@example.com'),
+            3 => array('id' => 3, 'name' => 'Scotty', 'email' => 'example3@example.com', 'fact' => array('hobbies' => array('fartings', 'bikes'))),
+        );
 
-        else
-        {
+        if ($users) {
+            $this->response($users, 200); // 200 being the HTTP response code
+        } else {
             $this->response(array('error' => 'Couldn\'t find any users!'), 404);
         }
     }
 
+    public function send_post() {
+        var_dump($this->request->body);
+    }
 
-	public function send_post()
-	{
-		var_dump($this->request->body);
-	}
+    public function send_put() {
+        var_dump($this->put('foo'));
+    }
 
-
-	public function send_put()
-	{
-		var_dump($this->put('foo'));
-	}
 }
