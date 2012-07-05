bf37537724e60ebc643d79a423c776b67f4820b8
Kevin Wood-Friend <kwoodfriend@hawthornpublications.com>
2012-07-05 14:15:30 -0400

    Moved GET, POST, PUT, and DELETE parsing to separate methods, allowing them to be overridden as needed

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -156,42 +156,7 @@ abstract class REST_Controller extends CI_Controller
 		// Some Methods cant have a body
 		$this->request->body = NULL;
 
-		switch ($this->request->method)
-		{
-			case 'get':
-				// Grab proper GET variables
-				parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $get);
-
-				// Merge both the URI segments and GET params
-				$this->_get_args = array_merge($this->_get_args, $get);
-				break;
-
-			case 'post':
-				$this->_post_args = $_POST;
-
-				$this->request->format and $this->request->body = file_get_contents('php://input');
-				break;
-
-			case 'put':
-				// It might be a HTTP body
-				if ($this->request->format)
-				{
-					$this->request->body = file_get_contents('php://input');
-				}
-
-				// If no file type is provided, this is probably just arguments
-				else
-				{
-					parse_str(file_get_contents('php://input'), $this->_put_args);
-				}
-
-				break;
-
-			case 'delete':
-				// Set up out DELETE variables (which shouldn't really exist, but sssh!)
-				parse_str(file_get_contents('php://input'), $this->_delete_args);
-				break;
-		}
+		$this->{'_parse_' . $this->request->method}();
 
 		// Now we know all about our request, let's try and parse the body if it exists
 		if ($this->request->format and $this->request->body)
@@ -766,6 +731,55 @@ abstract class REST_Controller extends CI_Controller
 		// Return false when there is an override value set but it does not match
 		// 'basic', 'digest', or 'none'. (the value was misspelled)
 		return false;
+	}
+
+	/**
+	 * Parse GET
+	 */
+	protected function _parse_get()
+	{
+		// Grab proper GET variables
+		parse_str(parse_url($_SERVER['REQUEST_URI'], PHP_URL_QUERY), $get);
+
+		// Merge both the URI segments and GET params
+		$this->_get_args = array_merge($this->_get_args, $get);
+	}
+
+	/**
+	 * Parse POST
+	 */
+	protected function _parse_post()
+	{
+		$this->_post_args = $_POST;
+
+		$this->request->format and $this->request->body = file_get_contents('php://input');
+	}
+
+	/**
+	 * Parse PUT
+	 */
+	protected function _parse_put()
+	{
+		// It might be a HTTP body
+		if ($this->request->format)
+		{
+			$this->request->body = file_get_contents('php://input');
+		}
+
+		// If no file type is provided, this is probably just arguments
+		else
+		{
+			parse_str(file_get_contents('php://input'), $this->_put_args);
+		}
+	}
+
+	/**
+	 * Parse DELETE
+	 */
+	protected function _parse_delete()
+	{
+		// Set up out DELETE variables (which shouldn't really exist, but sssh!)
+		parse_str(file_get_contents('php://input'), $this->_delete_args);
 	}
 
 	// INPUT FUNCTION --------------------------------------------------------------
