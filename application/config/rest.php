da1fc96c0258dd7f8ea4392a3a9907036bae1974
David Stanley <davidstanley01@gmail.com>
2012-09-12 08:24:15 -0500

    fixed some stuff
    changed default on rest keys.  also removed two columns from key table until I can figure out how i want to handle the request status.

--- application/config/rest.php
+++ application/config/rest.php
@@ -173,7 +173,7 @@ $config['rest_keys_table'] = 'keys';
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 |
 */
-$config['rest_enable_keys'] = TRUE;
+$config['rest_enable_keys'] = FALSE;
 
 /*
 |--------------------------------------------------------------------------
@@ -242,9 +242,7 @@ $config['rest_logs_table'] = 'logs';
 	  `api_key` varchar(40) NOT NULL,
 	  `ip_address` varchar(45) NOT NULL,
 	  `time` int(11) NOT NULL,
-	  `authorized` tinyint(1) NOT NULL,
-	  'status' int(3) NOT NULL,
-	  'payload' TEXT DEFAULT NULL,
+	  `authorized` tinyint(1) NOT NULL
 	  PRIMARY KEY (`id`)
 	) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 |
