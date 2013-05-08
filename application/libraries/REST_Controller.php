0c7bb496604d1afb302fd57aaa614593f3357f61
Dimitri De Franciscis <dimitri@megadix.it>
2013-05-08 17:54:16 +0200

    Fixed issue #117 - Undefined property $this->db

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -232,7 +232,7 @@ abstract class REST_Controller extends CI_Controller
 		}
 
 		// Use whatever database is in use (isset returns false)
-		elseif (@$this->db)
+		elseif (property_exists($this, "db"))
 		{
 			$this->rest->db = $this->db;
 		}
