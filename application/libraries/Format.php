b7fe4ab7aa0c571ec398c70f9783cc3dfde5ffa8
Chris Kacerguis <chris@cold.io>
2014-01-12 09:00:34 -0600

    resolves #235

--- application/libraries/Format.php
+++ application/libraries/Format.php
@@ -200,8 +200,13 @@ class Format {
 		$output = '"'.implode('","', $headings).'"'.PHP_EOL;
 		foreach ($data as &$row)
 		{
-			$row = str_replace('"', '""', $row); // Escape dbl quotes per RFC 4180
-			$output .= '"'.implode('","', $row).'"'.PHP_EOL;
+            if (is_array($row)) {
+                throw new Exception('Format class does not support multi-dimensional arrays');
+            } else {
+                $row    = str_replace('"', '""', $row); // Escape dbl quotes per RFC 4180
+                $output .= '"'.implode('","', $row).'"'.PHP_EOL;                
+            }
+
 		}
 
 		return $output;
