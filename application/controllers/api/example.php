26fb60742651353bd9efa465a9363f96b2326408
admin36 <github@reganlaitila.net>
2014-10-24 11:51:45 -0400

    Fix example

--- application/controllers/api/example.php
+++ application/controllers/api/example.php
@@ -28,7 +28,7 @@ class Example extends REST_Controller
         // within application/config/rest.php
         $this->methods['user_get']['limit'] = 500; //500 requests per hour per user/key
         $this->methods['user_post']['limit'] = 100; //100 requests per hour per user/key
-        $this->methods['user_delete']['limit'] = 50; //100 requests per hour per user/key
+        $this->methods['user_delete']['limit'] = 50; //50 requests per hour per user/key
     }
     
     function user_get()
