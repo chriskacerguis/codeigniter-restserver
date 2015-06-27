be00649c59c34232363b963d054876f134048197
softwarespot <softwarespotapps@gmail.com>
2015-06-27 08:18:24 +0300

    Added possible formats and defaulted to json
    json is now the most widely used format for api responses

--- application/config/rest.php
+++ application/config/rest.php
@@ -17,12 +17,19 @@ $config['force_https'] = FALSE;
 | REST Format
 |--------------------------------------------------------------------------
 |
-| What format should the data be returned in by default?
+| The default format of the response
 |
-|	Default: xml
+| 'array':      Array data structure
+| 'csv':        Comma separated file
+| 'json':       Uses json_encode(). Note: If a GET query string
+|               called 'callback' is passed then jsonp will be returned
+| 'html'        HTML using the table library
+| 'php':        Uses var_export()
+| 'serialized:  Uses serialize()
+| 'xml':        Uses simplexml_load_string()
 |
 */
-$config['rest_default_format'] = 'xml';
+$config['rest_default_format'] = 'json';
 
 /*
 |--------------------------------------------------------------------------
