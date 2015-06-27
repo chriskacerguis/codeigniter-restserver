d75377ccecbd815519cb111f69376927b2992ced
softwarespot <softwarespotapps@gmail.com>
2015-06-27 10:19:01 +0300

    Moved the 'no direct access' to a new line
    This is similar to CodeIgniter 3

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -1,4 +1,6 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
  * CodeIgniter Rest Controller
