df64259449d702a98f75b2b4cf18d4f265dc9957
softwarespot <softwarespotapps@gmail.com>
2015-07-02 21:40:24 +0300

    Updated parsing PUT variables function

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -1105,27 +1105,25 @@ abstract class REST_Controller extends CI_Controller {
     }
 
     /**
-     * Parse PUT
+     * Parse the PUT request arguments
      *
      * @access protected
+     * @return void
      */
     protected function _parse_put()
     {
-        // It might be a HTTP body
         if ($this->request->format)
         {
-            $this->request->body = file_get_contents('php://input');
+            $this->request->body = $this->input->raw_input_stream;
         }
-
-        // If no file type is provided, this is probably just arguments
         else
         {
+            // If no filetype is provided, then there are probably just arguments
             if ($this->input->method() === 'put')
             {
                 $this->_put_args = $this->input->input_stream();
             }
         }
-
     }
 
     /**
