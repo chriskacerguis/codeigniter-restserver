6bae80f77ab1ab7940e8d487aa437ad1aaaf15c7
softwarespot <softwarespotapps@gmail.com>
2015-07-17 08:59:33 +0300

    Added documentation for popular status codes and removed duplicates

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -16,37 +16,96 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 abstract class REST_Controller extends CI_Controller {
 
+    // Note: Only the widley used HTTP status codes are documented
+
+    // Informational
+
     const HTTP_CONTINUE = 100;
     const HTTP_SWITCHING_PROTOCOLS = 101;
     const HTTP_PROCESSING = 102;            // RFC2518
+
+    // Success
+
+    /**
+     * The request has succeeded
+     */
     const HTTP_OK = 200;
+
+    /**
+     * The server successfully created a new resource
+     */
     const HTTP_CREATED = 201;
     const HTTP_ACCEPTED = 202;
     const HTTP_NON_AUTHORITATIVE_INFORMATION = 203;
+
+    /**
+     * The server successfully processed the request, though no content is returned
+     */
     const HTTP_NO_CONTENT = 204;
     const HTTP_RESET_CONTENT = 205;
     const HTTP_PARTIAL_CONTENT = 206;
     const HTTP_MULTI_STATUS = 207;          // RFC4918
     const HTTP_ALREADY_REPORTED = 208;      // RFC5842
     const HTTP_IM_USED = 226;               // RFC3229
+
+    // Redirection
+
     const HTTP_MULTIPLE_CHOICES = 300;
     const HTTP_MOVED_PERMANENTLY = 301;
     const HTTP_FOUND = 302;
     const HTTP_SEE_OTHER = 303;
+
+    /**
+     * The resource has not been modified since the last request
+     */
     const HTTP_NOT_MODIFIED = 304;
     const HTTP_USE_PROXY = 305;
     const HTTP_RESERVED = 306;
     const HTTP_TEMPORARY_REDIRECT = 307;
     const HTTP_PERMANENTLY_REDIRECT = 308;  // RFC7238
+
+    // Client Error
+
+    /**
+     * The request cannot be fulfilled due to multiple errors
+     */
     const HTTP_BAD_REQUEST = 400;
+
+    /**
+     * The user is unauthorized to access the requested resource
+     */
     const HTTP_UNAUTHORIZED = 401;
     const HTTP_PAYMENT_REQUIRED = 402;
+
+    /**
+     * The requested resource is unavailable at this present time
+     */
     const HTTP_FORBIDDEN = 403;
+
+    /**
+     * The requested resource could not be found
+     *
+     * Note: This is sometimes used to mask if there was an UNAUTHORIZED (401) or
+     * FORBIDDEN (403) error, for security reasons
+     */
     const HTTP_NOT_FOUND = 404;
+
+    /**
+     * The request method is not supported by the following resource
+     */
     const HTTP_METHOD_NOT_ALLOWED = 405;
+
+    /**
+     * The request was not acceptable
+     */
     const HTTP_NOT_ACCEPTABLE = 406;
     const HTTP_PROXY_AUTHENTICATION_REQUIRED = 407;
     const HTTP_REQUEST_TIMEOUT = 408;
+
+    /**
+     * The request could not be completed due to a conflict with the current state
+     * of the resource
+     */
     const HTTP_CONFLICT = 409;
     const HTTP_GONE = 410;
     const HTTP_LENGTH_REQUIRED = 411;
@@ -65,7 +124,20 @@ abstract class REST_Controller extends CI_Controller {
     const HTTP_PRECONDITION_REQUIRED = 428;                                       // RFC6585
     const HTTP_TOO_MANY_REQUESTS = 429;                                           // RFC6585
     const HTTP_REQUEST_HEADER_FIELDS_TOO_LARGE = 431;                             // RFC6585
+
+    // Server Error
+
+    /**
+     * The server encountered an unexpected error
+     *
+     * Note: This is a generic error message when no specific message
+     * is suitable
+     */
     const HTTP_INTERNAL_SERVER_ERROR = 500;
+
+    /**
+     * The server does not recognised the request method
+     */
     const HTTP_NOT_IMPLEMENTED = 501;
     const HTTP_BAD_GATEWAY = 502;
     const HTTP_SERVICE_UNAVAILABLE = 503;
@@ -76,6 +148,7 @@ abstract class REST_Controller extends CI_Controller {
     const HTTP_LOOP_DETECTED = 508;                                               // RFC5842
     const HTTP_NOT_EXTENDED = 510;                                                // RFC2774
     const HTTP_NETWORK_AUTHENTICATION_REQUIRED = 511;
+
     /**
      * This defines the rest format.
      * Must be overridden it in a controller so that it is set.
@@ -263,88 +336,9 @@ abstract class REST_Controller extends CI_Controller {
      */
     protected $_enable_xss = FALSE;
 
-    // Success
-
-    /**
-     * The request has succeeded
-     */
-    const OK = 200;
-
-    /**
-     * The server successfully created a new resource
-     */
-    const CREATED = 201;
-
-    /**
-     * The server successfully processed the request, though no content is returned
-     */
-    const NO_CONTENT = 204;
-
-    // Redirection
-
-    /**
-     * The resource has not been modified since the last request
-     */
-    const NOT_MODIFIED = 304;
-
-    // Client Error
-
-    /**
-     * The request cannot be fulfilled due to multiple errors
-     */
-    const BAD_REQUEST = 400;
-
-    /**
-     * The user is unauthorized to access the requested resource
-     */
-    const UNAUTHORIZED = 401;
-
-    /**
-     * The requested resource is unavailable at this present time
-     */
-    const FORBIDDEN = 403;
-
-    /**
-     * The requested resource could not be found
-     *
-     * Note: This is sometimes used to mask if there was an UNAUTHORIZED (401) or
-     * FORBIDDEN (403) error, for security reasons
-     */
-    const NOT_FOUND = 404;
-
-    /**
-     * The request method is not supported by the following resource
-     */
-    const METHOD_NOT_ALLOWED = 405;
-
-    /**
-     * The request was not acceptable
-     */
-    const NOT_ACCEPTABLE = 406;
-
-    /**
-     * The request could not be completed due to a conflict with the current state
-     * of the resource
-     */
-    const CONFLICT = 409;
-
-    // Server Error
-
-    /**
-     * The server encountered an unexpected error
-     *
-     * Note: This is a generic error message when no specific message
-     * is suitable
-     */
-    const INTERNAL_SERVER_ERROR = 500;
-
-    /**
-     * The server does not recognised the request method
-     */
-    const NOT_IMPLEMENTED = 501;
-
     /**
      * HTTP status codes and their respective description
+     * Note: Only the widley used HTTP status codes are used
      *
      * @var array
      * @link http://www.restapitutorial.com/httpstatuscodes.html
