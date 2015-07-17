c42d60754f9676e8d1cbd25771d66a2ea116c40e
softwarespot <softwarespotapps@gmail.com>
2015-07-17 08:57:25 +0300

    Tidied comments

--- application/controllers/api/Example.php
+++ application/controllers/api/Example.php
@@ -24,11 +24,10 @@ class Example extends REST_Controller {
         parent::__construct();
 
         // Configure limits on our controller methods
-        // Ensure you have created the 'limits' table and enabled 'limits'
-        // within application/config/rest.php
-        $this->methods['user_get']['limit'] = 500; //500 requests per hour per user/key
-        $this->methods['user_post']['limit'] = 100; //100 requests per hour per user/key
-        $this->methods['user_delete']['limit'] = 50; //50 requests per hour per user/key
+        // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
+        $this->methods['user_get']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['user_post']['limit'] = 100; // 100 requests per hour per user/key
+        $this->methods['user_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
 
     function user_get()
