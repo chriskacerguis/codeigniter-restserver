1da1b9fbe183d5ced9d22c7c6cbc7b431adf19c7
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-07-23 17:24:49 +0300

    Adding a modified "Welcome" page and a homepage for quick tests.

---
+++ application/controllers/Rest_server.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rest_server extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->helper('url');
+
+        $this->load->view('rest_server');
+    }
+}
