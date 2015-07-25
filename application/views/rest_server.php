4293552b5ad5ac6a953e6b300069e97a126898c2
softwarespot <softwarespotapps@gmail.com>
2015-07-25 11:04:37 +0300

    Using official jQuery cdn

--- application/views/rest_server.php
+++ application/views/rest_server.php
@@ -112,7 +112,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.3.js"></script>
 
 <script>
     $(function() {
