06bedebd3f67a0e6057473ea2ee96de39075a8df
Kazuhito Higashioka <kmhigashioka@gmail.com>
2015-12-16 17:37:39 +0800

    Fix spacing

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -2157,31 +2157,31 @@ abstract class REST_Controller extends CI_Controller {
      * @return bool TRUE has expected keys in a request body; otherwise, FALSE
      */
     public function has_request_body($expected_keys = NULL)
-	{
-	    if (empty($this->request->body))
-	        return FALSE;
-
-	    $request_body = $this->request->body;
-	    $temp_keys = [];
-	    foreach ($request_body as $key => $value)
-	        $temp_keys[] = strtolower($key);
-	    $request_body = $temp_keys;
-
-	    if (is_object($expected_keys))
-		{
-	        $temp_keys = [];
-	        foreach ($expected_keys as $key => $value)
-	            $temp_keys[] = $key;
-	        $expected_keys = $temp_keys;
-	    }
-
-	    foreach ($expected_keys as $key)
-		{
-	        if (in_array(strtolower($key), $request_body))
-	            continue;
-	        return FALSE;
-	    }
-
-		return TRUE;
-	}
+    {
+        if (empty($this->request->body))
+            return FALSE;
+            
+        $request_body = $this->request->body;
+        $temp_keys = [];
+        foreach ($request_body as $key => $value)
+            $temp_keys[] = strtolower($key);
+        $request_body = $temp_keys;
+        
+        if (is_object($expected_keys))
+        {
+            $temp_keys = [];
+            foreach ($expected_keys as $key => $value)
+                $temp_keys[] = $key;
+            $expected_keys = $temp_keys;
+        }
+        
+        foreach ($expected_keys as $key)
+        {
+            if (in_array(strtolower($key), $request_body))
+                continue;
+            return FALSE;
+        }
+        
+        return TRUE;
+    }
 }
