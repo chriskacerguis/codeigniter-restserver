db979b16e3edf8aa5adbb48e569e80439f143e3b
Craine Runton <craine.runton@gmail.com>
2015-12-21 21:02:02 -0700

    Fix descriptions and whitespace cleanup

--- application/config/rest.php
+++ application/config/rest.php
@@ -517,8 +517,8 @@ $config['rest_language'] = 'english';
 |--------------------------------------------------------------------------
 |
 | Set to true to enable Cross-Origin Resource Sharing (CORS). Useful if you
-|  are hosting your API on a different domain from the appolication that 
-|  will access it through a browser. 
+|  are hosting your API on a different domain from the appolication that
+|  will access it through a browser.
 |
 */
 $config['check_cors'] = false;
@@ -529,7 +529,7 @@ $config['check_cors'] = false;
 |--------------------------------------------------------------------------
 |
 | Set to true to enable Cross-Origin Resource Sharing (CORS) from any
-|  source domain 
+|  source domain
 |
 */
 $config['allow_any_cors_domain'] = false;
@@ -539,11 +539,10 @@ $config['allow_any_cors_domain'] = false;
 | CORS Allowable Domains
 |--------------------------------------------------------------------------
 |
-| Used if $config['check_cors'] is set to true and 
-|  $config['allow_any_cors_domain'] is set to false. Set all the allowable 
-|  domains within the array. 
+| Used if $config['check_cors'] is set to true and $config['allow_any_cors_domain']
+|  is set to false. Set all the allowable domains within the array.
 |
-| e.g. $config['allowed_origins'] = ['www.example.com', 'mobile.example.com']
+| e.g. $config['allowed_origins'] = ['www.example.com', 'spa.example.com']
 |
 */
 $config['allowed_cors_origins'] = [];
