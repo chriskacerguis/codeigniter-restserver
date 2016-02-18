6e675db4a34bcbdc31397bb50536c886dda3d92f
softwarespot <softwarespotapps@gmail.com>
2016-02-18 22:20:08 +0200

    Fixed basepath check

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,6 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+    defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
