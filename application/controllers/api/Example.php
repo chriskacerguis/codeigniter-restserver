bc7fe511ed11c0488d1e43115e7f04002759f47c
TGoC <florincm@ymail.com>
2016-09-10 21:29:46 +0100

    typo - example rate limit

--- application/controllers/api/Example.php
+++ application/controllers/api/Example.php
@@ -25,9 +25,9 @@ class Example extends REST_Controller {
 
         // Configure limits on our controller methods
         // Ensure you have created the 'limits' table and enabled 'limits' within application/config/rest.php
-        $this->methods['user_get']['limit'] = 500; // 500 requests per hour per user/key
-        $this->methods['user_post']['limit'] = 100; // 100 requests per hour per user/key
-        $this->methods['user_delete']['limit'] = 50; // 50 requests per hour per user/key
+        $this->methods['users_get']['limit'] = 500; // 500 requests per hour per user/key
+        $this->methods['users_post']['limit'] = 100; // 100 requests per hour per user/key
+        $this->methods['users_delete']['limit'] = 50; // 50 requests per hour per user/key
     }
 
     public function users_get()
