09f2e5b2c67b0c9e5d7dbb31153301faeeac1eaa
Amit Moryossef <amitmoryossef@gmail.com>
2016-10-29 10:44:45 +0300

    Removed paths inspection in the project

--- application/controllers/api/Key.php
+++ application/controllers/api/Key.php
@@ -3,6 +3,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 // This can be removed if you use __autoload() in config.php OR use Modular Extensions
+/** @noinspection PhpIncludeInspection */
 require APPPATH . '/libraries/REST_Controller.php';
 
 /**
