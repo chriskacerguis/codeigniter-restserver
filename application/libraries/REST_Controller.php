b06bc614fe13fbe8889d08e4c8712e38412a3f73
Amit Moryossef <amitmoryossef@gmail.com>
2016-10-29 10:40:22 +0300

    Removed unnecessary return doc

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -375,7 +375,6 @@ abstract class REST_Controller extends CI_Controller {
      * @access public
      * @param string $config Configuration filename minus the file extension
      * e.g: my_rest.php is passed as 'my_rest'
-     * @return void
      */
     public function __construct($config = 'rest')
     {
