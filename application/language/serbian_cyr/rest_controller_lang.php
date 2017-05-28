10be576dcedd236d018d2aa7d6b50f7bf7ba9d63
Goran Grbic <tpojka@gmail.com>
2017-05-29 00:01:53 +0200

    Additional translation set

--- application/language/serbian_cyr/rest_controller_lang.php
+++ application/language/serbian_cyr/rest_controller_lang.php
@@ -13,6 +13,6 @@ $lang['text_rest_ajax_only'] = 'Једино *AJAX* захтеви су дозв
 $lang['text_rest_api_key_unauthorized'] = 'Овај *API* кључ нема овлашћења за захтевани контролер';
 $lang['text_rest_api_key_permissions'] = 'Овај *API* кључ нема дозвољен степен овлашћења';
 $lang['text_rest_api_key_time_limit'] = 'Овај *API* кључ је прекорачио временски лимит за дати метод';
-$lang['text_rest_ip_address_time_limit'] = 'This IP Address has reached the time limit for this method';//todo translate
+$lang['text_rest_ip_address_time_limit'] = 'Ова *IP* адреса је прекорачила временски лимит за дати метод';//todo translate
 $lang['text_rest_unknown_method'] = 'Непознат метод';
 $lang['text_rest_unsupported'] = 'Неподржан протокол';
