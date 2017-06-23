859eb0156ca92ca55adf3baf09968b34bb8016cd
Himphen Hui <himphen@gmail.com>
2017-06-24 07:51:13 +0800

    Update rest_controller_lang.php (#799)

--- application/language/traditional-chinese/rest_controller_lang.php
+++ application/language/traditional-chinese/rest_controller_lang.php
@@ -13,6 +13,6 @@ $lang['text_rest_ajax_only'] = '只有 AJAX 類型請求被允許';
 $lang['text_rest_api_key_unauthorized'] = '這個 API 金鑰沒有辦法存取指定的 controller';
 $lang['text_rest_api_key_permissions'] = '這個 API 金鑰沒有具備足夠權限';
 $lang['text_rest_api_key_time_limit'] = '這個 API 金鑰已經超過有效期限';
-$lang['text_rest_ip_address_time_limit'] = 'This IP Address has reached the time limit for this method';//todo translate
+$lang['text_rest_ip_address_time_limit'] = '這個 IP 位置的流量已經超過上限';
 $lang['text_rest_unknown_method'] = '未知的方法';
 $lang['text_rest_unsupported'] = '不支援的通訊協定';
