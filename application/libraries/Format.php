514fce4f22dfc8fb284b72f4b5cae62bf484f1da
MasonFI <joonas_manninen@hotmail.com>
2017-07-23 17:56:35 +0300

    Added encode conversion to to_csv-function so that special characters are shown correctly in MS Excel. (#818)

--- application/libraries/Format.php
+++ application/libraries/Format.php
@@ -378,6 +378,9 @@ class Format {
 
         // Close the handle
         fclose($handle);
+        
+        // Convert UTF-8 encoding to UTF-16LE which is supported by MS Excel
+        $csv = mb_convert_encoding($csv, 'UTF-16LE', 'UTF-8');
 
         return $csv;
     }
