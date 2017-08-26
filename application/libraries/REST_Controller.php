d29223b5e39389c8e8d532cfa34eeeb511eb6002
Fabian Hanisch <kontakt@hanisch-it.de>
2017-08-26 11:27:02 +0200

    Add backward compatibility for Format

--- application/libraries/REST_Controller.php
+++ application/libraries/REST_Controller.php
@@ -409,10 +409,15 @@ abstract class REST_Controller extends \CI_Controller {
         $this->get_local_config($config);
 
         // At present the library is bundled with REST_Controller 2.5+, but will eventually be part of CodeIgniter (no citation)
-        $this->load->library('format');
-        if(!class_exists('Format')) {
+        if(class_exists('Format'))
+        {
             $this->format = new Format();
         }
+        else
+        {
+             $this->load->library('format');
+        }
+
 
         // Determine supported output formats from configuration
         $supported_formats = $this->config->item('rest_supported_formats');
