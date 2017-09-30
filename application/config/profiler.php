a5bbf6feb713ca2ec77d4522e0db60d48ffb2274
Zeeshan <zgmunshi@gmail.com>
2017-09-30 21:46:45 +0530

    code igniter profiler default parameters 
    code igniter profiler default parameters 

--- application/config/profiler.php
+++ application/config/profiler.php
@@ -12,3 +12,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/profiling.html
 |
 */
+$config['benchmarks']           = TRUE;
+$config['config']               = TRUE;
+$config['controller_info']      = TRUE;
+$config['get']                  = TRUE;
+$config['http_headers']         = TRUE;
+$config['memory_usage']         = TRUE;
+$config['post']                 = TRUE;
+$config['queries']              = TRUE;
+$config['eloquent']             = FALSE;
+$config['uri_string']           = TRUE;
+$config['view_data']            = TRUE;
+$config['query_toggle_count']   = 100;
