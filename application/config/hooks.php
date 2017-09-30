be8ba6951f3640d3d3811c149b22aff75d41d105
Zeeshan <zgmunshi@gmail.com>
2017-09-30 21:46:12 +0530

    hook post_controller_constructor
    hook for enabling / disabling the profiler through out the application 

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+// hook for enable/disable profiling
+$hook['post_controller_constructor'][] = array(
+	'class'    => 'ProfilerEnabler',
+	'function' => 'enableProfiler',
+	'filename' => 'hooks.profiler.php',
+	'filepath' => 'hooks',
+	'params'   => array()
+);
