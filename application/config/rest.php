2c1c621ca79ff91a4a3f5bbe1c7aa6748b56ad27
Mattias Sandström <1013635+tangix@users.noreply.github.com>
2018-03-08 20:39:58 +0100

    Add config to force CORS pre-flight headers to be included (#886)
    * Added config option to force setting of CORS pre-flight headers.
    
    * Updated the setting to be empty by default and updated the comment.

--- application/config/rest.php
+++ application/config/rest.php
@@ -604,3 +604,24 @@ $config['allow_any_cors_domain'] = FALSE;
 |
 */
 $config['allowed_cors_origins'] = [];
+
+/*
+|--------------------------------------------------------------------------
+| CORS Forced Headers
+|--------------------------------------------------------------------------
+|
+| If using CORS checks, always include the headers and values specified here 
+| in the OPTIONS client preflight.
+| Example:
+| $config['forced_cors_headers'] = [
+|   'Access-Control-Allow-Credentials' => 'true'
+| ];
+|
+| Added because of how Sencha Ext JS framework requires the header
+| Access-Control-Allow-Credentials to be set to true to allow the use of
+| credentials in the REST Proxy. 
+| See documentation here:
+| http://docs.sencha.com/extjs/6.5.2/classic/Ext.data.proxy.Rest.html#cfg-withCredentials
+|
+*/
+$config['forced_cors_headers'] = [];
