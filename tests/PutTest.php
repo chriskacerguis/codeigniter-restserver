52a49e221b43b581a2e2bfd1b31975f09a558a77
Chris Kacerguis <chris@fuzzyblender.com>
2019-02-20 20:07:59 -0600

    added put test

---
+++ tests/PutTest.php
@@ -0,0 +1,44 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class PutTest extends TestCase
+{
+    private $http;
+
+    public function setUp() : void
+    {
+        $this->http = new GuzzleHttp\Client(['base_uri' => 'http://localhost:8080']);
+    }
+
+    public function tearDown() : void
+    {
+        $this->http = null;
+    }
+
+    public function testPut()
+    {
+        $data = [
+            'key1' => 'value1',
+            'key2' => 'value2'
+        ];
+        $response   = $this->http->post('/api/test', ['form_params' => $data]);
+        $rdata      = json_decode($response->getBody(), true);
+        $this->assertEquals($data['key1'], $rdata['key1']);
+        $this->assertEquals($data['key2'], $rdata['key2']);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+
+    public function testPutWithKey()
+    { // @todo fix when we have the key
+        $data = [
+            'key1' => 'value1',
+            'key2' => 'value2'
+        ];
+        $response   = $this->http->post('/api/test', ['form_params' => $data]);
+        $rdata      = json_decode($response->getBody(), true);
+        $this->assertEquals($data['key1'], $rdata['key1']);
+        $this->assertEquals($data['key2'], $rdata['key2']);
+        $this->assertEquals(200, $response->getStatusCode());
+    }
+}
