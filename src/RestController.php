7d0339e6004d3d7d5427e3a00b9b4767287694d3
Felipe Gubert <felipe.gubert@gmail.com>
2021-03-24 13:26:32 -0300

    resolve issue #1060

--- src/RestController.php
+++ src/RestController.php
@@ -18,6 +18,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 class RestController extends \CI_Controller
 {
     /**
+     * Defines the http error codes.
+     * 
+     * @var array
+     */
+    protected $http_status = [
+                                'HTTP_OK' => 200,
+                                'HTTP_CREATED' => 201,
+                                'HTTP_NOT_MODIFIED' => 304,
+                                'HTTP_BAD_REQUEST' => 400,
+                                'HTTP_UNAUTHORIZED' => 401,
+                                'HTTP_FORBIDDEN' => 403,
+                                'HTTP_NOT_FOUND' => 404,
+                                'METHOD_NOT_ALLOWED' => 405,
+                                'HTTP_NOT_ACCEPTABLE' => 406,
+                                'HTTP_INTERNAL_ERROR' => 500,
+                             ];
+    /**
      * This defines the rest format
      * Must be overridden it in a controller so that it is set.
      *
