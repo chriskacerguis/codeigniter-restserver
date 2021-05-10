952989023b685127cac47ce62aec2169108ced18
Alexandr Toropov <toropoviste@yandex.ru>
2021-05-10 22:13:59 +0300

    Add russian language file for REST controller

---
+++ language/russian/rest_controller_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * Russian language
+ */
+
+$lang['text_rest_invalid_api_key'] = 'Недопустимый API ключ %s'; // %s is the REST API key
+$lang['text_rest_invalid_credentials'] = 'Неверные учетные данные';
+$lang['text_rest_ip_denied'] = 'IP отказано в доступе';
+$lang['text_rest_ip_unauthorized'] = 'IP неавторизованный';
+$lang['text_rest_unauthorized'] = 'Неавторизованный';
+$lang['text_rest_ajax_only'] = 'Разрешены только AJAX-запросы';
+$lang['text_rest_api_key_unauthorized'] = 'Этот ключ API не имеет доступа к запрошенному контроллеру';
+$lang['text_rest_api_key_permissions'] = 'У этого ключа API недостаточно разрешений';
+$lang['text_rest_api_key_time_limit'] = 'Истек срок действия ключа API для этого метода';
+$lang['text_rest_ip_address_time_limit'] = 'Этот IP-адрес достиг предела времени для этого метода';
+$lang['text_rest_unknown_method'] = 'Неизвестный метод';
+$lang['text_rest_unsupported'] = 'Неподдерживаемый протокол';
